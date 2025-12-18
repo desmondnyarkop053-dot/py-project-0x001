8a3b906432821a36ed1e915c8ae4c66106452aa2
desmondnyarkop053-dot <desmondnyarkop053@gmail.com>
2025-12-18 06:50:45 -0800

    frimpong

--- hello.py
+++ hello.py
@@ -5,3 +5,12 @@ except ValueError:
     print('Invalid value')
 
 print("hello world")
+
+
+name=input('Name:')
+age=int(input('Age:'))
+score=float(input('Score:'))
+subject=input('Subject:')
+school=input('School:')
+country=input('Country:')
+print(f'Hello {name}, you are {age} years old, your score in {subject} is {score}, you study at {school} in {country}.')
