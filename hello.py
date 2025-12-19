ff76131504a32dee809f432129182294cc8c495a
desmondnyarkop053-dot <desmondnyarkop053@gmail.com>
2025-12-18 16:32:40 -0800

    :desmond

--- hello.py
+++ hello.py
@@ -1,16 +1,36 @@
-try:
-    age=int(input('Age:'))
-    print(age)
-except ValueError:
-    print('Invalid value')
+#1.Add
+#2.subtract
+#3.multiply
+#4.divide
+print("Select operation to perform:")
+print("1. Add")
+print("2.  subtract")
+print("3. Mulbtiply")
+print("4. divide")
+print("5. Exit")
 
-print("hello world")
+operation = input()
+
+if operation in ['1', '2', '3', '4']:
+    num1 =float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == '1':
+        print(f"{num1} + {num2} = {num1 + num2}")
+
+    elif operation == '2':
+        print(f"{num1} - {num2} = {num1 - num2}")
+
+    elif operation == '3':
+        print(f"{num1} * {num2} = {num1 * num2}")
+
+    elif operation == '4':
+        if num2 != 0:
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Error! Division by zero.")  
 
 
-name=input('Name:')
-age=int(input('Age:'))
-score=float(input('Score:'))
-subject=input('Subject:')
-school=input('School:')
-country=input('Country:')
-print(f'Hello {name}, you are {age} years old, your score in {subject} is {score}, you study at {school} in {country}.')
+
+            course='python for beginners'
+            print(course[5:0])
